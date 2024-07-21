8f278347f2874decdb3694664b4820c8e19308de
xuliulei <xuliulei666@gmail.com>
2024-07-21 15:56:40 +0800

    fix: update the kw

--- server/api/music/kw-search.ts
+++ server/api/music/kw-search.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
 
     try {
         let query = await getQuery(event)
-        let res: any = await $fetch('https://netdisk.kkwnhub.com/search', {
+        let res: any = await $fetch('https://kwapi.kkwnhub.com/search', {
             method: 'GET',
             query: query
         })
--- server/api/music/kw-url.ts
+++ server/api/music/kw-url.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
 
     try {
         let query = await getQuery(event)
-        let res: any = await $fetch('https://netdisk.kkwnhub.com/url', {
+        let res: any = await $fetch('https://kwapi.kkwnhub.com/url', {
             method: 'GET',
             query: query
         })
