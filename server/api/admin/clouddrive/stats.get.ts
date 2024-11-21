34cca50c90af020f6c4a2edee75726f995479e21
lei <ahagwybwqs@gmail.com>
2024-11-21 13:37:35 +0800

    fix issue

--- server/api/admin/clouddrive/stats.get.ts
+++ server/api/admin/clouddrive/stats.get.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '~/lib/prisma'
 
 export default defineEventHandler(async (event) => {
     // 验证用户
