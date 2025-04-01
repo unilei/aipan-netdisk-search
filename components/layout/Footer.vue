41aa5b1e13584f05149612e51dc82ad557f34ba1
lei <ahagwybwqs@gmail.com>
2025-04-01 08:59:48 +0800

    fix bugs

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -7,7 +7,7 @@ const footerLinks = [
 </script>
 
 <template>
-  <footer class="bg-white dark:bg-slate-800 dark:text-white py-6 space-y-2">
+  <footer class="bg-slate-100 dark:bg-slate-800 dark:text-white py-6 space-y-2">
 
     <!-- 版权信息 -->
     <p class="text-center text-xs sm:text-sm">
